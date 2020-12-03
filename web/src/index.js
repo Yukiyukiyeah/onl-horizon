eae1b746d2a825a69ac103ffa338284a4a490e76
Yang Luo <hsluoyz@qq.com>
2020-12-03 14:20:51 +0800

    Use local storage for AAD.

--- web/src/index.js
+++ web/src/index.js
@@ -10,12 +10,17 @@ import { MsalProvider } from "@azure/msal-react";
 import { PublicClientApplication } from "@azure/msal-browser";
 
 // MSAL configuration
+// https://github.com/AzureAD/microsoft-authentication-library-for-js/blob/46886ff0856ce02e0d2113525ccb05bb68f92a93/lib/msal-browser/docs/configuration.md
 const configuration = {
   auth: {
     clientId: "d0984d5d-b8cf-42b9-9002-3a11685c34d6",
     redirectUri: "http://localhost:2000/home", //defaults to application start page
     postLogoutRedirectUri: "http://localhost:2000",
-  }
+  },
+  cache: {
+    cacheLocation: "localStorage",
+    storeAuthStateInCookie: true,
+  },
 };
 
 const pca = new PublicClientApplication(configuration);
