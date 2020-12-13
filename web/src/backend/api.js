6decc58ebca4c5bd93fa8745a5a34c0e10326622
Yang Luo <hsluoyz@qq.com>
2020-12-13 23:42:07 +0800

    Improve api.js style.

--- web/src/backend/api.js
+++ web/src/backend/api.js
@@ -12,55 +12,48 @@ export const sendCreateJobReq = (params = {}) => {
   for (const key of Object.keys(params)) {
     createJobDefaultParams[key] = params[key];
   }
-  return post(baseUrl + '/jobs', createJobDefaultParams);
+  return post(`${baseUrl}/jobs`, createJobDefaultParams);
 };
 
 // get one job info
 export const getJobInfo = (jobId) => {
-  const infoUrl = baseUrl + '/display/jobDetail/' + jobId;
-  return get(infoUrl);
+  return get(`${baseUrl}/display/jobDetail/${jobId}`);
 };
 
 // get job info list
 export const allJobInfo = (userId = Setting.getUserId()) => {
-  const url = baseUrl + '/display/jobList';
   const param = {'userId': userId};
-  return get(url, param);
+  return get(`${baseUrl}/display/jobList`, param);
 };
 
 // delete job
 export const deleteJob = (jobId, data = {}, header = {}) => {
-  const deleteJobId = baseUrl + '/jobs/' + jobId;
-  return deleteData(deleteJobId, data, header);
+  return deleteData(`${baseUrl}/jobs/${jobId}`, data, header);
 };
 
 // restart job
 export const restartJob = (jobId, params = {}) => {
-  const restartJobUrl = baseUrl + '/' + jobId + '/restart';
-  return post(restartJobUrl, params);
+  return post(`${baseUrl}/${jobId}/restart`, params);
 };
 
 // stop job
 const defaultParam = '[{"value": 1,"path": "status", "op": "replace"}]';
 export const stopJob = (jobId, params = defaultParam) => {
-  const stopJobUrl = baseUrl + '/jobs?id=' + jobId;
-
-  return patch(stopJobUrl, params);
+  return patch(`${baseUrl}/jobs?id=${jobId}`, params);
 };
 
 // download dataset
 //GET http://opennetlab.org/api/results/download/e0000001-0000-0000-0000-000000000001?filename=webrtc_send_20201119082250.log,
 export const downloadDataset = (jobId, filename) => {
-  const downloadUrl = baseUrl + '/results/download/' + jobId;
   const param = {'filename': filename};
-  return get(downloadUrl, param);
+  return get(`${baseUrl}/results/download/${jobId}`, param);
 };
 
 // get user name by user id
-export const getUserName = (userId = '00000000-0000-0000-0000-000000000001') => {
-  const url = baseUrl + '/users' + '/' + userId;
-  return get(url);
-};
+// export const getUserName = (userId = '00000000-0000-0000-0000-000000000001') => {
+//   const url = baseUrl + '/users' + '/' + userId;
+//   return get(url);
+// };
 
 // run job
 export const runApp = (jobId, appName, params) => { //
@@ -70,9 +63,7 @@ export const runApp = (jobId, appName, params) => { //
 
 // get user ID
 export const getUserId = () => {
-  const infoUrl = baseUrl + '/auth';
-  return get(infoUrl)
-    // .then(res => res.json())
+  return get(`${baseUrl}/auth`)
     .then((res) => {
       const userId = res.id;
       localStorage.setItem("userId", userId);
