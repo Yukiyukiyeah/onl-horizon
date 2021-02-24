aa6e01d2e7337f82a44a0aa8455c77af6b71708c
Yuki <yukitang0703@gmail.com>
2021-02-24 17:06:24 +0800

    sign up UI

--- web/src/pages/account/SignUpPage.js
+++ web/src/pages/account/SignUpPage.js
@@ -1,11 +1,13 @@
-import React from "react";
+import React, {useState} from "react";
 import AdvInput from "../../components/AdvInput";
-import {Row, Column, Col, Upload, Button} from 'antd';
+import {Row, Column, Col, Button} from 'antd';
 import * as Setting from "../../utils/Setting";
 import {getUser} from "../../backend/api";
+import '../../styles/SignUpPage.scss';
 
 const SignUpPage = (props) => {
-
+  const [title, setTitle] = useState('');
+  const onHandleClick = () => {};
   return(
     <div className="signup-container">
       <Row className="landing-header" justify="space-between">
@@ -18,31 +20,81 @@ const SignUpPage = (props) => {
           <a href="/contact">CONTACT US</a>
         </div>
       </Row>
-      <Row className="signup-panel-container">
-        <Col span={20}>
-          <p className="title">Supplement Information</p>
-          <div className="model-name">
-            <AdvInput
-              type="normal"
-              title="Name"
-              placeholder="Model Name"
-              widthRange={[200, 1200]}
-              errorText="Please enter your name"
-              isAdaptive={true}
-              height="40px"
-            />
-          </div>
-
-          <Row className="model">
-            <AdvInput
-              type="normal"
-              title="Model Upload"
-              widthRange={[200, 400]}
-              errorText="Please upload your model"
-              isAdaptive={false}
-              height="40px"
-              disabled={true}
-            />
+      <Row className="signup-panel-container" justify="center" align="middle" >
+        <Col className="signup-panel-info">
+          <p className="supplementary-information">Supplement Information</p>
+          <Row className="row" justify="space-between">
+            <div className="firstname">
+              <AdvInput
+                type="normal"
+                title="First Name"
+                placeholder="First Name"
+                widthRange={[200, 400]}
+                errorText="Please enter first name"
+                isAdaptive={true}
+                height="40px"
+              />
+            </div>
+            <div className="lastname">
+              <AdvInput
+                type="normal"
+                title="Last Name"
+                widthRange={[200, 400]}
+                errorText="Please enter last name"
+                isAdaptive={true}
+                height="40px"
+              />
+            </div>
+            <div className="company">
+              <AdvInput
+                type="normal"
+                title="University/Institution"
+                widthRange={[200, 400]}
+                errorText="Please enter university/institution"
+                isAdaptive={true}
+                height="40px"
+              />
+            </div>
+          </Row>
+          <Row className="row" justify="space-between">
+            <div className="email">
+              <AdvInput
+                type="normal"
+                title="Email"
+                widthRange={[440, 800]}
+                errorText="Please enter Email"
+                isAdaptive={true}
+                height="40px"
+              />
+            </div>
+            <div className="Phone">
+              <AdvInput
+                type="normal"
+                title="Phone"
+                widthRange={[200, 400]}
+                errorText="Please enter phone number"
+                isAdaptive={true}
+                height="40px"
+              />
+            </div>
+          </Row>
+          <Row className="row" justify="space-between">
+            <div className="lastname">
+              <AdvInput
+                type="select"
+                title="Title"
+                size="large"
+                options={['Dr.', 'Mr.', 'Mrs.', 'Miss.']}
+                optionsValue={['Dr.', 'Mr.', 'Mrs.', 'Miss.']}
+                errorText={"Please select your title"}
+                handleChange={setTitle}
+                isAdaptive={true}
+                widthRange={[200, 1200]}
+              />
+            </div>
+          </Row>
+          <Row className="row" justify="center">
+            <Button className="signup-button" type="primary" onClick={onHandleClick}>SIGN UP NOW</Button>
           </Row>
         </Col>
       </Row>
