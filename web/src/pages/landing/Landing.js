3cef42c3f3a63464b03cd23a62aa138c36dec7f6
Yuki <yukitang0703@gmail.com>
2021-03-04 10:53:49 +0800

    adjust landing layout

--- web/src/pages/landing/Landing.js
+++ web/src/pages/landing/Landing.js
@@ -55,7 +55,7 @@ const Landing = () => {
 
   return (
     <div className="landing-container">
-      <Row className="landing-header" justify="space-between">
+      <Row className="landing-header" justify="space-between" align="top">
         <div className="landing-header-container">
           <div className="landing-logo">
             {
@@ -83,70 +83,72 @@ const Landing = () => {
           </ul>
         </div>
       </Row>
-      <Row className="home-hero">
-        <div className="bg">
-          <p className="bg-title">OpenNetLab</p>
-          <p className="bg-description">OpenNetLab is to build
-            and provide a distributed networking platform with many collaborative nodes
-            and a common benchmarking dataset (a.k.a ImageNet in Networking Area)
-            through this platform for the researchers to collect real networking data and
-            training/evaluating their AI models for various networking environment,
-            including Internet/cloud, wireless and mobile network.<br/>
-          <img src={loginButton} className="login" onClick={handleLogin} />
-          </p>
-        </div>
+      <div className="landing-content-container">
+        <Row className="home-hero">
+          <div className="bg">
+            <p className="bg-title">OpenNetLab</p>
+            <p className="bg-description">OpenNetLab is to build
+              and provide a distributed networking platform with many collaborative nodes
+              and a common benchmarking dataset (a.k.a ImageNet in Networking Area)
+              through this platform for the researchers to collect real networking data and
+              training/evaluating their AI models for various networking environment,
+              including Internet/cloud, wireless and mobile network.<br/>
+            <img src={loginButton} className="login" onClick={handleLogin} />
+            </p>
+          </div>
 
-      </Row>
-      <Row className="chat" type="flex" justify="center" align="middle">
-        <Col
-          span={10} class="text right margin-right">
-          <p className="title">Contribute to research comunity</p>
-          <p className="description">Platform - Free to networking research community</p>
-          <p className="description">Dataset - Publish to GitHub for researcher to reproduce the experiments and train
-            models</p>
-        </Col>;
-        <Col span={10} class="img-container">
-          <img className="img" src={chat} />
-        </Col>;
-      </Row>
-      <Row class="data" type="flex" justify="center" align="middle">
-        <Col
-          span={10} class="img-container right margin-right">
-          <img className="img " src={data} />
-        </Col>
-        <Col
-          span={10} class="text left">
-          <p className="title">Data Centric for networking-related AI</p>
-          <p className="description">Automatic data collection, data storing, data aggregation, and data sharing</p>
-        </Col>;
-      </Row>
-      <Row class="application" type="flex" justify="center" align="middle">
-        <Col
-          span={10} class="text right margin-right">
-          <p className="title">Real applications</p>
-          <p className="description">Real Time Communication (RTC)</p>
-          <p className="description">Video/Data Streaming</p>
-          <p className="description">Web Service/CDN</p>
-          <p className="description">Other networking applications</p>
-        </Col>;
-        <Col span="10" class="img-container">
-          <img className="img" src={realtime} />
-        </Col>;
-      </Row>
-      <Row className="map" type="flex" justify="center" align="middle">
-        <Col
-          span={10} class="img-container right margin-right">
-          <img className="img" src={map} />
-        </Col>
-        <Col
-          span={10} class="text left">
-          <p className="title">Distributed heterogenous test nodes</p>
-          <p className="description">Geo-distributed testbed</p>
-          <p className="description">Heterogenous network (LAN, wireless, 4G/5G, etc)</p>
-          <p className="description">Web Service/CDN</p>
-          <p className="description">Various user devices (desktop, Mobile, etc)</p>
-        </Col>;
-      </Row>
+        </Row>
+        <Row className="chat" type="flex" justify="center" align="middle">
+          <Col
+            span={10} class="text right margin-right">
+            <p className="title">Contribute to research comunity</p>
+            <p className="description">Platform - Free to networking research community</p>
+            <p className="description">Dataset - Publish to GitHub for researcher to reproduce the experiments and train
+              models</p>
+          </Col>;
+          <Col span={10} class="img-container">
+            <img className="img" src={chat} />
+          </Col>;
+        </Row>
+        <Row class="data" type="flex" justify="center" align="middle">
+          <Col
+            span={10} class="img-container right margin-right">
+            <img className="img " src={data} />
+          </Col>
+          <Col
+            span={10} class="text left">
+            <p className="title">Data Centric for networking-related AI</p>
+            <p className="description">Automatic data collection, data storing, data aggregation, and data sharing</p>
+          </Col>;
+        </Row>
+        <Row class="application" type="flex" justify="center" align="middle">
+          <Col
+            span={10} class="text right margin-right">
+            <p className="title">Real applications</p>
+            <p className="description">Real Time Communication (RTC)</p>
+            <p className="description">Video/Data Streaming</p>
+            <p className="description">Web Service/CDN</p>
+            <p className="description">Other networking applications</p>
+          </Col>;
+          <Col span="10" class="img-container">
+            <img className="img" src={realtime} />
+          </Col>;
+        </Row>
+        <Row className="map" type="flex" justify="center" align="middle">
+          <Col
+            span={10} class="img-container right margin-right">
+            <img className="img" src={map} />
+          </Col>
+          <Col
+            span={10} class="text left">
+            <p className="title">Distributed heterogenous test nodes</p>
+            <p className="description">Geo-distributed testbed</p>
+            <p className="description">Heterogenous network (LAN, wireless, 4G/5G, etc)</p>
+            <p className="description">Web Service/CDN</p>
+            <p className="description">Various user devices (desktop, Mobile, etc)</p>
+          </Col>;
+        </Row>
+      </div>
     </div>
   );
 };
